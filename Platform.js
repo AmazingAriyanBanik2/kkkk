573b3e47a3474bfb4a6860344d0ba622b6ffefea
AmazingAriyanBanik2 <86153940+AmazingAriyanBanik2@users.noreply.github.com>
2022-02-14 20:56:57 +0530

    Add files via upload

---
+++ Platform.js
@@ -0,0 +1,7 @@
+class Platform extends BaseClass{
+    constructor(x, y, width,height){
+        super(x,y,width,300);
+        this.image = loadImage("sprites/gharhai-removebg-preview.png");
+      }
+    
+    }
